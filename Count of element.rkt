50c9131c82c170ab5f71436cb1165ddfd069e57f
Simona <croft883@gmail.com>
2021-05-26 13:03:49 +0300

    Update Count of element.rkt

--- Count of element.rkt
+++ Count of element.rkt
@@ -1,9 +1,1 @@
-#lang racket
-
-;Намира броя на срещанията на X елемент в списъка:
-(define(count_elem x lst)
-	(cond((null? lst)0)
-	((equal? x(car lst))
-	(+ 1(count_elem x(cdr lst)))
-	(else(count_elem x(cdr lst))))))
 
